fff766b7a671c6bb6544b61acaa4348aac9a62e2
Rufus Russell <rufusrussell@gmail.com>
2022-12-12 18:03:22 +0000

    minimum code to recreate bug after npx create-expo-app

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  DatePickerInput,
+  registerTranslation,
+  en,
+} from "react-native-paper-dates";
 
-export default function App() {
+registerTranslation("en", en);
+
+function App() {
+  const [date, setDate] = useState(undefined);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <DatePickerInput value={date} onChange={(d) => setDate(d)} />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
